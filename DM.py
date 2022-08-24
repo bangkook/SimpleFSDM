063edef0923dcff5f81e4977140e200e05b227f5
bangkook <m.hossam2551@gmail.com>
2022-08-24 22:40:59 +0200

    implement SET function

--- DM.py
+++ DM.py
@@ -43,22 +43,39 @@ def parse_args():
 
 args = parse_args()
 
+parent_dir = "C:/Users/cyber/Desktop/SimpleFSDM/"
+
 def create(schema):
     f = open(schema, "r")
     data = json.load(f)
     dir = data['database_name']
-    parent_dir = "C:/Users/cyber/Desktop/SimpleFSDM/"
     path = os.path.join(parent_dir, dir)
     os.mkdir(path)
     for i in data['Tables']:
         t_path = os.path.join(path, i['name'])
         os.mkdir(t_path)
 
+def set(db, table, value):
+    disallowed_characters = "{\":},"
+    for character in disallowed_characters:
+	    value = value.replace(character, "")
+    s = value.split(" ")
+    
+    dictionary = {}
+    for i in range(0, len(s)-1, 2):
+        dictionary[s[i]] = s[i+1]
+    id = s[1]
+    data = json.dumps(dictionary, indent=4)
+    with open(db + "/" + table + "/" + id + ".json", "w") as outfile:
+        outfile.write(data)
+    print(value, s)
+
+
 if(args.command == "CREATE"): 
    create(args.schema)
 
-#if(args.command == "SET"):
- #   set(args.db, args.t, args.v)
+if(args.command == "SET"):
+   set(args.database, args.table, args.value)
 
 #if(args.command == "GET"):
  #   get(args.db, args.t, args.pk, args.v)
