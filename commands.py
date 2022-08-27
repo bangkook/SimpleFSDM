85b880fafc0143164d824801c4c7205771c4d723
bangkook <m.hossam2551@gmail.com>
2022-08-27 13:38:56 +0200

    Update set function

--- commands.py
+++ commands.py
@@ -18,27 +18,46 @@ def create(schema):
         os.mkdir(t_path)
 
 
+def create_raw(db, table, pk):
+    path = parent_dir + "/" + db + "/" + table + "/" + pk + ".json"
+    f = open(path, "w")
+
+    data = json.load(open("schema.json", "r"))
+
+    for tab in data['Tables']:
+        if tab['name'] == table:
+            f.write(json.dumps({tab['primary_key']: pk}, indent=4))
+
+    f.close()
+
+# if pk not found, create new file, else update it
 def set(db, table, pk, value):
     if pk == None:
         raise Exception("PrimaryKeyIsMissing")
+
+    path = parent_dir + "/" + db + "/" + table + "/" + pk + ".json"
+
+    if not os.path.exists(path):
+        create_raw(db, table, pk)
+
+    f = open(path, "r")
+    data = json.load(f)
+    f.close()
+    
+    if(value == None):
+        return
 
     disallowed_characters = "{\":},"
     for character in disallowed_characters:
 	    value = value.replace(character, "")
     s = value.split(" ")
-    
-    path = parent_dir + "/" + db + "/" + table + "/" + pk + ".json"
-
-    if os.path.exists(path):
-        raise Exception("ItemExistsWithSamePK")
-
-    dictionary = {}
+     
     for i in range(0, len(s)-1, 2):
-        dictionary[s[i]] = s[i+1]
+        data[s[i]] = s[i+1]
 
-    data = json.dumps(dictionary, indent=4)
-    with open(path, "w") as outfile:
-        outfile.write(data)
+    f = open(path, "w")
+    json.dump(data, f)
+    f.close()
 
 
 def get(db, table, pk, value):
