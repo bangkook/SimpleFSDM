2a9aad0d776153f97ad9759c728aafd02ae34c12
bangkook <m.hossam2551@gmail.com>
2022-09-05 23:03:32 +0200

    update command_factory.py

--- commands/command_factory.py
+++ commands/command_factory.py
@@ -1,7 +1,10 @@
 from commands.create_command import CreateCommand
+from commands.exceptions import *
 
 class CommandFactory:
     def create(self, args):
         cmd = args.command.lower()
         if (cmd == "create"):
             return CreateCommand(args.schema_path)
+        else:
+            raise InvalidParameterError("command is wrong or missing")
