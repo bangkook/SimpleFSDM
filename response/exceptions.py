ea5e7e1785c8fd28589a1a3f2bcc4af2aaeaf018
A.Wael <96313471+AbdelrahmanWael2@users.noreply.github.com>
2022-09-15 08:56:48 +0200

    Update exceptions.py

--- response/exceptions.py
+++ response/exceptions.py
@@ -29,3 +29,18 @@ class ColumnsNotExistInSchema(Exception):
     def __init__(self, message):
         self.status = Status.ColumnsNotExistInSchema
         super().__init__(message)
+        
+class DatabaseNotFound(Exception):
+    def __init__(self, message):
+        self.status = Status.DatabaseNotExist
+        super().__init__(message)
+        
+class TableNotFound(Exception):
+    def __init__(self, message):
+        self.status = Status.TableNotExist
+        super().__init__(message)
+        
+ class ValueNotFound(Exception):
+    def __init__(self, message):
+        self.status = Status.ValueNotExist
+        super().__init__(message)
