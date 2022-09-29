91ba8752a093bfc722cac3759939dbef9873ba81
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2022-09-30 01:31:34 +0200

    try to fix

---
+++ SimpleFSDM-SET_COMMAND/commands/test_create.py
@@ -0,0 +1,52 @@
+import json
+import os
+import sys
+import unittest
+from schema_keys import SchemaKeys
+from create_command import CreateCommand
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+schema_path = os.path.join(root_dir, os.path.join("tests", "schema.json"))
+sys.path.append(root_dir)
+
+
+class Test_TestCommands(unittest.TestCase):
+    def test_wrong_input(self):
+        wrong_schema = "schema.json"
+        self.assertRaises(Exception, CreateCommand, wrong_schema)
+
+    def test_no_input(self):
+        self.assertRaises(Exception, CreateCommand, None)
+
+    def test_create_database(self):
+        db_name = "Check-in"
+        db_path = os.path.join(root_dir, db_name)
+        self.assertEqual(os.path.exists(db_path), True)
+
+    def test_create_tables(self):
+        db_path = os.path.join(root_dir, "Check-in")
+        data = None
+        with open(schema_path, "r") as schema_file:
+            data = json.load(schema_file)
+        for table in data[SchemaKeys.TABLES]:
+            table_path = os.path.join(db_path, table[SchemaKeys.NAME])
+            self.assertEqual(os.path.exists(table_path), True)
+
+    def test_wrong_db_name(self):
+        db_name = "Check"
+        db_path = os.path.join(root_dir, db_name)
+        table = "Flights_Details"
+        table_path = os.path.join(db_path, table)
+        self.assertEqual(os.path.exists(table_path), False)
+
+    def test_create_multiple_time(self):
+        db_name = "Check-in"
+        db_path = os.path.join(root_dir, db_name)
+        for i in range(3):
+            CreateCommand(schema_path).execute()
+            self.assertEqual(os.path.exists(db_path), True)
+
+
+if __name__ == '__main__':
+    CreateCommand(schema_path).execute()
+    unittest.main()
--- SimpleFSDM-SET_COMMAND/model/file_mode.py
+++
@@ -1,6 +0,0 @@
-import enum
-
-
-class FileMode(enum.Enum):
-    x = False
-    w = True
