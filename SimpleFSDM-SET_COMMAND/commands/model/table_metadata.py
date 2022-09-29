c5279a5b610029bf51300e29a450f92ec0039595
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2022-09-29 04:00:48 +0200

    fix

--- SimpleFSDM-SET_COMMAND/commands/model/table_metadata.py
+++ SimpleFSDM-SET_COMMAND/commands/model/table_metadata.py
@@ -2,9 +2,10 @@ from index import *
 from schema_keys import *
 from response.exceptions import *
 
+
 class TableMetaData:
     def __init__(self, table):
-        TableMetaData.__validate(table.table_schema)
+        TableMetaData.__validate__(table.table_schema)
         self.__path = table.get_path()
         self.name = table.table_schema[SchemaKeys.NAME]
         self.primary_key = table.table_schema[SchemaKeys.PRIMARY_KEY]
@@ -23,13 +24,15 @@ class TableMetaData:
 
     @staticmethod
     def __validate__(table_schema):
-        if table_schema[SchemaKeys.PRIMARY_KEY] is None or table_schema[SchemaKeys.PRIMARY_KEY] not in table_schema[SchemaKeys.COLUMNS]:
+        if table_schema[SchemaKeys.PRIMARY_KEY] is None or table_schema[SchemaKeys.PRIMARY_KEY] not in table_schema[
+            SchemaKeys.COLUMNS]:
             raise InvalidParameterError("Primary_key not found")
 
     @staticmethod
     def __create_table_schema(table_schema, path):
         indices = table_schema[SchemaKeys.INDEX_KEYS]
-        table_schema.update({SchemaKeys.INDEX_KEYS: TableMetaData.get_indices_names(table_schema[SchemaKeys.INDEX_KEYS])})
+        table_schema.update(
+            {SchemaKeys.INDEX_KEYS: TableMetaData.get_indices_names(table_schema[SchemaKeys.INDEX_KEYS])})
         with open(os.path.join(path, "{}_schema.json".format(table_schema[SchemaKeys.NAME])), 'w') as file:
             json.dump(table_schema, file)
         table_schema.update({SchemaKeys.INDEX_KEYS: indices})
@@ -44,7 +47,7 @@ class TableMetaData:
         for index_name in index_keys:
             indices_names.append(index_name)
         return indices_names
-    
+
     @staticmethod
     def get_table_schema(path, table_name):
         with open(os.path.join(path, "{}_schema.json".format(table_name)), 'r') as file:
